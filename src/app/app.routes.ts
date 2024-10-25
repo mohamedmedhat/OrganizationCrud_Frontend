6ce8bc862cd16276a723769270e0b10e5ce6a1b2
moMedhat <mmr973320@gmail.com>
2024-10-25 22:40:43 +0300

    [add]: make auth routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,36 @@
 import { Routes } from '@angular/router';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { AppComponent } from './app.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'auth',
+    title: 'auth',
+    children: [
+      {
+        path: 'register',
+        title: 'regisetr',
+        loadComponent: () =>
+          import('./register/register.component').then(
+            (r) => r.RegisterComponent
+          ),
+      },
+      {
+        path: 'login',
+        title: 'login',
+        loadComponent: () =>
+          import('./login/login.component').then((l) => l.LoginComponent),
+      },
+    ],
+  },
+  {
+    path: '',
+    title: 'home',
+    component: AppComponent,
+  },
+  {
+    path: '**',
+    title: 'page not found',
+    component: NotFoundComponent,
+  },
+];
